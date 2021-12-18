1296c7b9bb503b84f2bd4d29807615d27d0a42fd
Gorana Gojic <gorana.gojic.dev@gmail.com>
2021-12-18 18:36:19 +0100

    Major data aggregation modifications in aggregate.py script.
    1. Added survey type as a field in the output file (can be 'regular' or 'control').
    2. Changed former 'correctAnswer' field in 'correctAnswers' so now there can be multiple disease labels associated with an image.
    3. A field 'correct' is set to True if doctor's answer is one of 'correctAnswers'.
    4. aggregate_dr now replaces every occurence of 'background_diabetic_retinopathy' with 'diabetic_retinopathy' regardless of the column.

--- aggregate.py
+++ aggregate.py
@@ -21,38 +21,61 @@ def aggregate(survey_dir, master_fp, out_fp, aggregate_dr):
     :param survey_dir: Where to find all survey result files. It is expected that results are in JSON file format.
     :param master_fp: A path to the master file. Should be in CSV file format.
     :param out_fp: Where to save the aggregated results.
-    :param aggregate_dr: Consider diabetic retinopathy and background diabetic retinopathy as same diseases.
-    :return:
+    :param aggregate_dr: Consider diabetic retinopathy and background diabetic retinopathy as same diseases. If this
+        option is active, every occurrence of `background_diabetic_retinopathy` will be replaced with
+        `diabetic retinopathy`.
+    :return: None
     """
+    if aggregate_dr:
+        print(">> An option to treat equally 'background_diabetic_retinopathy' and 'diabetic_retionopathy' is ENABLED.")
+
     input_path: Path = Path(survey_dir)
     output: pd.DataFrame = pd.DataFrame([], [], [
         "surveyID",
+        "surveyType",
         "questionID",
         "doctorID",
         "network",
         "dataset",
         "imageName",
         "answer",
-        "correctAnswer",
+        "correctAnswers",
         "correct",
         "certainty"
     ])
 
     master = pd.read_csv(master_fp)
 
+    entry_id = 1
+
     for file in input_path.glob("*.json"):
+
+        # name of the survey indicates if it is regular or control (name examples: "regular survey 1.csv",
+        # "control survey 1.json", etc.
+        if "regular" in str(file):
+            survey_type = "regular"
+        else:
+            survey_type = "control"
+
         with open(file) as fd:
             alterations = []
             loaded = json.load(fd)
-            res_count, entries = loaded["ResultCount"], loaded["Data"]
+            res_count, entries = loaded["ResultCount"], loaded["Data"]  # ResCount - n entries, Data - entry content
             for entry in entries:
                 doctor = entry["doctorID"]
                 for key in entry.keys():
+                    # parse surveyID and questionID
+                    # string example used for parsing: '"s1-q39-choice": "arteriosclerotic_retinopathy"'
                     m = re.match(r's(\d+)-q(\d+)-choice', key)
                     if m:
                         s = m.group(1)
                         q = m.group(2)
+
+                        # parse certainty, string example: `"s1-q39-certainty": 3`
                         certainty = entry[f's{s}-q{q}-certainty']
+
+                        # each question in the survey is associated with one segmentation mask - parse mask info from
+                        # master file, string example: "001041-vgan-stare.png" (imgid-network-dataset)
                         img = master[
                             (master['survey_id'] == int(s)) & (master['question_id'] == int(q))].image_filename.array[0]
                         mm = re.match(r'\d+-(\w+)-(\w+).*', img)
@@ -61,28 +84,37 @@ def aggregate(survey_dir, master_fp, out_fp, aggregate_dr):
                             sys.exit()
                         network = mm.group(1)
                         dataset = mm.group(2)
-                        correct_answer = \
-                        master[(master['survey_id'] == int(s)) & (master['question_id'] == int(q))].disease_token.array[
-                            0]
+
+                        # get correct labels from master file for a segmentation mask, one image can have multiple
+                        # diseases associated with it
+                        correct_answers = \
+                        list(master[(master['survey_id'] == int(s)) & (master['question_id'] == int(q))].disease_token)
                         our_answer = entry[key]
-                        answer_correct = correct_answer == our_answer
+
+                        # it seems that background_diabetic_retinopathy is subtype or a synonim for diabetic_retinopathy
+                        # this is not verified at the moment, but this code piece is here to account on possibility that
+                        # these two disease types should be treated as one
                         if aggregate_dr:
-                            if our_answer == "diabetic_retinopathy" and \
-                                    correct_answer == "background_diabetic_retinopathy":
-                                answer_correct = True
-                            if our_answer == "background_diabetic_retinopathy" and \
-                                    correct_answer == "diabetic_retinopathy":
-                                answer_correct = True
+                            correct_answers = ["diabetic_retinopathy" if a == "background_diabetic_retinopathy" else a
+                                               for a in correct_answers]
+                            our_answer = "diabetic_retinopathy" \
+                                if our_answer == "background_diabetic_retinopathy" else our_answer
+
+                        # see if survey answer is a correct one
+                        # if there are multiple diseases associated with an image, if a doctor has selected one of the
+                        # diseases as his answer, the answer is considered to be correct
+                        answer_correct = our_answer in correct_answers
 
                         alterations.append({
                             "surveyID": int(s),
+                            "surveyType": survey_type,
                             "questionID": int(q),
                             "doctorID": int(doctor),
                             "network": network,
                             "dataset": dataset,
                             "imageName": img,
                             "answer": our_answer,
-                            "correctAnswer": correct_answer,
+                            "correctAnswers": ','.join(correct_answers),
                             "correct": answer_correct,
                             "certainty": int(certainty)
                         })
